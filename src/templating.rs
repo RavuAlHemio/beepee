69e55bd110f0404e874d10be2e2b8b51df5777dd
Ondřej Hošek <ondra.hosek@gmail.com>
2021-09-07 10:19:28 +0200

    format mmol/l with 1 decimal place and mg/dl with 0

--- src/templating.rs
+++ src/templating.rs
@@ -7,7 +7,12 @@ use tera;
 
 pub(crate) struct RatioToFloat;
 impl tera::Filter for RatioToFloat {
-    fn filter(&self, value: &serde_json::Value, _args: &HashMap<String, serde_json::Value>) -> tera::Result<serde_json::Value> {
+    fn filter(&self, value: &serde_json::Value, args: &HashMap<String, serde_json::Value>) -> tera::Result<serde_json::Value> {
+        let digits = match args.get("digits") {
+            Some(serde_json::Value::Number(n)) => n.as_u64().map(|u| u as usize),
+            Some(_) => return Err(tera::Error::msg("\"digits\" must be a number")),
+            None => None,
+        };
         let vstr = if let Some(vs) = value.as_str() {
             vs.to_owned()
         } else {
@@ -17,6 +22,11 @@ impl tera::Filter for RatioToFloat {
             .map_err(|pre| tera::Error::msg(pre))?;
         let num = *ratio.numer() as f64;
         let den = *ratio.denom() as f64;
-        Ok(serde_json::Value::from(num / den))
+        let out_text = if let Some(d) = digits {
+            format!("{:.*}", d, num / den)
+        } else {
+            format!("{}", num / den)
+        };
+        Ok(serde_json::Value::from(out_text))
     }
 }
